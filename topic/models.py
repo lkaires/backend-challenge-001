50cf490f8fae4b5a3e836aaaa119e23943319793
Helena Aires <lenakaires@gmail.com>
2021-02-26 16:50:41 -0300

    auto_create on url_name in Topic model

--- topic/models.py
+++ topic/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from helpers.models import BaseInfoModel
 
 class Topic(BaseInfoModel):
-    url_name = models.SlugField(default='', unique=True, primary_key=True)
+    url_name = models.SlugField(default='', unique=True, primary_key=True, null=False, auto_created=True)
